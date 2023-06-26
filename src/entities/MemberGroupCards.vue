8fdc86906c8b858923a569970e660f0586b55160
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2023-06-26 13:31:32 +0300

    add All cards click handler

--- src/entities/MemberGroupCards.vue
+++ src/entities/MemberGroupCards.vue
@@ -8,13 +8,18 @@ const productStore = useProductStore();
 
 const props = defineProps({
     product_id: Number,
-    product_index: Number
+    product_index: Number,
 });
 </script>
 
 <template>
     <b-list-group class="d-flex flex-row my-2">
-        <MemberCard :label="'Все'" />
+        <MemberCard
+            :label="'Все'"
+            :flag="productStore.products[product_index].flags['All']"
+            :product_id="product_id"
+            :member_id="'All'"
+        />
         <div class="ms-1" v-for="member in memberStore.homies" :key="member.id">
             <MemberCard
                 :label="member.name"
--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,81 +1,93 @@
 import { defineStore } from "pinia"
 import { useMemberStore } from "./MemberStore"
 
-
-
 export const useProductStore = defineStore("productStore", {
     state: () => ({
         products: [
-            {   
+            {
                 id: 0,
-                name: 'Секретный Персонаж',
+                name: "Секретный Персонаж",
                 cost: 15,
                 flags: {
                     0: false,
                     1: true,
-                    2: false
-                }
+                    2: false,
+                },
             },
             {
                 id: 1,
-                name: 'Адиль',
+                name: "Адиль",
                 cost: 15,
                 flags: {
                     0: false,
                     1: true,
-                    2: true
-                }
+                    2: true,
+                },
             },
-            {   
+            {
                 id: 2,
-                name: 'Игорь',
+                name: "Игорь",
                 cost: 15,
                 flags: {
                     0: true,
                     1: true,
-                    2: false
-                }
-            }
-        ]
+                    2: false,
+                },
+            },
+        ],
     }),
     actions: {
         addNewProduct(member_ids) {
             let obj = {}
-            member_ids.map(member_id => {
+            obj["All"] = false
+            member_ids.map((member_id) => {
                 obj[member_id] = false
             })
-            this.products = [...this.products, {id: Date.now(), name: "", cost: "", flags: {}}]
-            this.products.map(product => {
-                product.flags = {...product.flags, ...obj}
-                
+            this.products = [
+                ...this.products,
+                { id: Date.now(), name: "", cost: "", flags: {} },
+            ]
+            this.products.map((product) => {
+                product.flags = { ...product.flags, ...obj }
             })
             console.log(this.products)
         },
         deleteProduct(id) {
-            this.products = this.products.filter(item => item.id !== id)
+            this.products = this.products.filter((item) => item.id !== id)
         },
         addProductName(id, name) {
-            this.products.map(item => {
-                if(item.id === id) {
+            this.products.map((item) => {
+                if (item.id === id) {
                     item.name = name
                 }
             })
             console.log(this.products)
         },
         addProductCost(id, cost) {
-            this.products.map(item => {
-                if(item.id === id) {
+            this.products.map((item) => {
+                if (item.id === id) {
                     item.cost = cost
                 }
             })
             console.log(this.homies)
         },
         changeFlag(product_id, member_id) {
-            this.products.map(product => {
-                if(product.id === product_id) {
-                    product.flags[member_id] = !product.flags[member_id]
-                }
-            })
-        }
-    }
+            if (member_id === "All") {
+                this.products.map(item => {
+                    if (item.id === product_id) {
+                        item.flags['All'] = !item.flags['All']
+                        Object.keys(item.flags).map(id => {
+                            item.flags[id] = item.flags['All']
+                        })
+                    }
+                })
+            } else {
+                this.products.map((product) => {
+                    if (product.id === product_id) {
+                        product.flags[member_id] = !product.flags[member_id]
+                    }
+                })
+            }
+        },
+    },
 })
