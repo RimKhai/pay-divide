bd052b360de88612ace6af2652dcde7833ce1b44
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2023-07-10 15:21:01 +0300

    Create dev branch

---
+++ src/shared/NextButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+    routeName: String,
+    label: String,
+    isDisabled: Boolean
+});
+</script>
+
+<template>
+    <div class="next w-100">
+        <RouterLink :to="(isDisabled) ? {} : {name: routeName }">
+            <BButton :disabled="isDisabled" class="button button__next">{{ (isDisabled) ? "Введите поля!" : label }}</BButton>
+        </RouterLink>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.button__next {
+    border-radius: $pill;
+    width: 40*$ui-step;
+    border-color: $additional-color;
+    color: $text-color;
+    background: $additional-color;
+    &:hover {
+        background: $hover-color;
+    }
+}
+</style>
